2bc7e91b6581a921f5397706126f0c55ac539c84
enumivo <admin@enumivo.org>
2018-10-24 23:19:58 +0800

    Update ex.cpp

--- ex.cpp
+++ ex.cpp
@@ -36,7 +36,7 @@ void ex::buy(const currency::transfer &transfer) {
 
   eln_supply = eln_supply/10000;
 
-  double buy = eln_supply*pow(1+(received/enu_balance,0.01)-1);
+  double buy = eln_supply*(pow(1+(received/enu_balance), 0.01) - 1);
   
   auto to = transfer.from;
 
