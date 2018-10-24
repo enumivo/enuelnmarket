d1b231c43a267ce845c6bae0906be8662007e880
enumivo <admin@enumivo.org>
2018-10-24 22:50:43 +0800

    Initial commit

---
+++ ex.cpp
@@ -0,0 +1,113 @@
+#include "ex.hpp"
+
+#include <cmath>
+#include <enulib/action.hpp>
+#include <enulib/asset.hpp>
+#include "enu.token.hpp"
+
+using namespace enumivo;
+using namespace std;
+
+void ex::buy(const currency::transfer &transfer) {
+  if (transfer.to != _self) {
+    return;
+  }
+
+  // get ENU balance
+  double enu_balance = enumivo::token(N(enu.token)).
+	   get_balance(_self, enumivo::symbol_type(ENU_SYMBOL).name()).amount;
+  
+  enu_balance = enu_balance/10000;
+
+  double received = transfer.quantity.amount;
+  received = received/10000;
+
+  enu_balance = enu_balance-received;  
+
+  // get ELN balance
+  double eln_balance = enumivo::token(N(eln.coin)).
+	   get_balance(_self, enumivo::symbol_type(ELN_SYMBOL).name()).amount;
+
+  eln_balance = eln_balance/10000;
+
+  double buy = eln_balance*(exp(received/enu_balance)-1);
+
+  auto to = transfer.from;
+
+  auto quantity = asset(10000*buy, ELN_SYMBOL);
+
+  action(permission_level{_self, N(active)}, N(eln.coin), N(transfer),
+         std::make_tuple(_self, to, quantity,
+                         std::string("Buy ELN with ENU")))
+      .send();
+}
+
+void ex::sell(const currency::transfer &transfer) {
+  if (transfer.to != _self) {
+    return;
+  }
+
+  // get ELN balance
+  double eln_balance = enumivo::token(N(eln.coin)).
+	   get_balance(_self, enumivo::symbol_type(ELN_SYMBOL).name()).amount;
+  
+  eln_balance = eln_balance/10000;
+
+  double received = transfer.quantity.amount;
+  received = received/10000;
+
+  eln_balance = eln_balance-received;  
+
+  // get ENU balance
+  double enu_balance = enumivo::token(N(enu.token)).
+	   get_balance(_self, enumivo::symbol_type(ENU_SYMBOL).name()).amount;
+
+  enu_balance = enu_balance/10000;
+
+  double sell = enu_balance*(exp(received/eln_balance)-1);
+
+  auto to = transfer.from;
+
+  auto quantity = asset(10000*sell, ENU_SYMBOL);
+
+  action(permission_level{_self, N(active)}, N(enu.token), N(transfer),
+         std::make_tuple(_self, to, quantity,
+                         std::string("Sell ELN for ENU")))
+      .send();
+}
+
+void ex::apply(account_name contract, action_name act) {
+  if (contract == N(enu.token) && act == N(transfer)) {
+    auto transfer = unpack_action_data<currency::transfer>();
+    enumivo_assert(transfer.quantity.symbol == ENU_SYMBOL,
+                 "Must send ENU");
+    buy(transfer);
+    return;
+  }
+
+  if (contract == N(eln.coin) && act == N(transfer)) {
+    auto transfer = unpack_action_data<currency::transfer>();
+    enumivo_assert(transfer.quantity.symbol == ELN_SYMBOL,
+                 "Must send ELN");
+    sell(transfer);
+    return;
+  }
+
+  if (act == N(transfer)) {
+    auto transfer = unpack_action_data<currency::transfer>();
+    enumivo_assert(false, "Must send ELN or ENU");
+    sell(transfer);
+    return;
+  }
+
+  if (contract != _self) return;
+
+}
+
+extern "C" {
+[[noreturn]] void apply(uint64_t receiver, uint64_t code, uint64_t action) {
+  ex enueln(receiver);
+  enueln.apply(code, action);
+  enumivo_exit(0);
+}
+}
